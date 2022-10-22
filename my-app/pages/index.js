18f0aa801e3c30d7adf1d713e0032f97e5d1e507
Mr Stegosaurus <mrstego123@gmail.com>
2022-10-22 13:02:38 -0400

    add counter for # of whitelistd

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -203,6 +203,9 @@ export default function Home() {
           <div className={styles.description}>
             Its an NFT collection for developers in Crypto.
           </div>
+          <div className="styles.description">
+            {numberOfWhitelisted} have already joined the Whitelist
+          </div>
           {renderButton()}
         </div>
         <div>
